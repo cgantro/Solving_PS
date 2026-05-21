d5267080f940649cbc329044c9d25dfd25708da8
yoonpyo hong <84065357+cgantro@users.noreply.github.com>
2026-05-21 11:22:15 +0900

    Create 1-TwoSum

---
+++ LeetCode/1-TwoSum/1-TwoSum.py
@@ -0,0 +1,12 @@
+# Last updated: 2026. 5. 21. 오전 11:22:14
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        print(hashmap)
+        for i in range(len(nums)):
+            complement = target-nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i,hashmap[complement]]
+        return []
